d52a1232a59d465a8f00109cd1de4847c2b64fdd
ben awad <benawad97@gmail.com>
2017-07-24 22:52:43 -0500

    create a flatlist and load data from an external api

---
+++ src/index.js
@@ -0,0 +1,43 @@
+import React, { Component } from "react";
+import { FlatList, StyleSheet, Text, View } from "react-native";
+
+export default class App extends Component {
+  state = {
+    data: []
+  };
+
+  componentWillMount() {
+    this.fetchData();
+  }
+
+  fetchData = async () => {
+    const response = await fetch("https://randomuser.me/api?results=500");
+    const json = await response.json();
+    this.setState({ data: json.results });
+  };
+
+  render() {
+    return (
+      <View style={styles.container}>
+        <FlatList
+          data={this.state.data}
+          keyExtractor={(x, i) => i}
+          renderItem={({ item }) =>
+            <Text>
+              {`${item.name.first} ${item.name.last}`}
+            </Text>}
+        />
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    marginTop: 15,
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "#F5FCFF"
+  }
+});
