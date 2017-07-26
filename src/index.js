6b98b300c816213fc6b71f92d22403fe25f5fba7
ben awad <benawad97@gmail.com>
2017-07-25 22:01:24 -0500

    add styling to list

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
-import { FlatList, StyleSheet, Text, View } from "react-native";
+import { FlatList, Text, View } from "react-native";
+import { List, ListItem } from "react-native-elements";
 
 export default class App extends Component {
   state = {
@@ -11,33 +12,27 @@ export default class App extends Component {
   }
 
   fetchData = async () => {
-    const response = await fetch("https://randomuser.me/api?results=500");
+    const response = await fetch("https://randomuser.me/api?results=10");
     const json = await response.json();
     this.setState({ data: json.results });
   };
 
   render() {
     return (
-      <View style={styles.container}>
-        <FlatList
-          data={this.state.data}
-          keyExtractor={(x, i) => i}
-          renderItem={({ item }) =>
-            <Text>
-              {`${item.name.first} ${item.name.last}`}
-            </Text>}
-        />
+      <View>
+        <List>
+          <FlatList
+            data={this.state.data}
+            keyExtractor={(x, i) => i}
+            renderItem={({ item }) =>
+              <ListItem
+                roundAvatar
+                avatar={{ uri: item.picture.thumbnail }}
+                title={`${item.name.first} ${item.name.last}`}
+              />}
+          />
+        </List>
       </View>
     );
   }
 }
-
-const styles = StyleSheet.create({
-  container: {
-    marginTop: 15,
-    flex: 1,
-    justifyContent: "center",
-    alignItems: "center",
-    backgroundColor: "#F5FCFF"
-  }
-});
